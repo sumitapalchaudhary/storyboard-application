0279ec836727fef8d6485b5c66228b59c6a5bcdc
Sumita Palchaudhary <sumita.palchaudhary@gmail.com>
2021-11-19 20:30:42 +0100

    story board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoryBoardComponent } from './story-board/story-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoryBoardComponent
   ],
   imports: [
     BrowserModule,
